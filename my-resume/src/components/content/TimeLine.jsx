b8e3bcb272f8a3f357bc1a7d78b96c57f1821cd9
kaniapaulina <kaniapaulina2308@gmail.com>
2026-03-29 23:28:52 +0200

    made the next round of tweaks and fixed vercel deployment

--- my-resume/src/components/content/TimeLine.jsx
+++ my-resume/src/components/content/TimeLine.jsx
@@ -1,10 +1,11 @@
+import React from 'react';
+
 const TimelineItem = ({ title, organization, date, description, type }) => (
   <div className="timeline-view">
     <div className="timeline-line"></div>
     <div className="timeline-dot"></div>
-
+    <span className="timeline-date">{date}</span>
     <div className="timeline-card">
-      <span className="timeline-date">{date}</span>
       <h3 className="item-title">{title}</h3>
       <h4 className="timeline-organization">{organization}</h4>
       <p className="timeline-description">
@@ -12,26 +13,26 @@ const TimelineItem = ({ title, organization, date, description, type }) => (
       </p>
     </div>
   </div>
-  );
+);
 
 
-const Timeline = ({title, experiences}) => {
-    return (
+const Timeline = ({ title, experiences }) => {
+  return (
     <section className="timeline-mainview">
-        <h2 className="timeline-title">{title}</h2>
-        <div className="timeline-content">
-            {experiences?.map((experiences, index) => (
-                <TimelineItem 
-                key={index} 
-                date={experiences.date}
-                title={experiences.title}
-                organization={experiences.organization}
-                description={experiences.description}
-                />
-            ))}
-        </div>
+      <h2 className="timeline-title">{title}</h2>
+      <div className="timeline-content">
+        {experiences?.map((experiences, index) => (
+          <TimelineItem
+            key={index}
+            date={experiences.date}
+            title={experiences.title}
+            organization={experiences.organization}
+            description={experiences.description}
+          />
+        ))}
+      </div>
     </section>
-    );
+  );
 };
 
 export default Timeline;
